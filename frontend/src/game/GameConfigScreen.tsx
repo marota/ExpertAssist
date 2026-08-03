f7b8bf65313a010d0c3d449c5b92d4af9838f541
marota <antoine-marot@live.fr>
2026-08-03 15:23:12 +0200

    test redeploy
    Signed-off-by: marota <antoine-marot@live.fr>

--- frontend/src/game/GameConfigScreen.tsx
+++ frontend/src/game/GameConfigScreen.tsx
@@ -3,7 +3,7 @@
 // If a copy of the Mozilla Public License, version 2.0 was not distributed with this file,
 // you can obtain one at http://mozilla.org/MPL/2.0/.
 // SPDX-License-Identifier: MPL-2.0
-// This file is part of Co-Study4Grid a Power Grid Study tool Assistant Interface to help solve contigencies for a grid state under study.
+// This file is part of Co-Study4Grid a Power Grid Study tool Assistant Interface to help solve contigencies for a grid state under study. 
 
 import { useEffect, useMemo, useRef, useState } from 'react';
 import { api } from '../api';
